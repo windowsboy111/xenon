c25f36c57a612a62620ab62b149d531c433fc151
windowsboy111 <cyruschan0111@gmail.com>
2020-11-02 23:15:52 +0800

    added guide for installation

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ src/xenon.cpp `pkg-config --libs gtk+-3.0` `pkg-config --cflags gtk+-3.0` `pkg-config --libs webkit2gtk-4.0` `pkg-config --cflags webkit2gtk-4.0` -std=c++17
+g++ -o xenon src/xenon.cpp `pkg-config --libs gtk+-3.0` `pkg-config --cflags gtk+-3.0` `pkg-config --libs webkit2gtk-4.0` `pkg-config --cflags webkit2gtk-4.0` -std=c++17
